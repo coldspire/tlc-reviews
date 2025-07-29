7248044e889676167bd18033023d80a482dd248e
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-07-29 13:20:21 -0400

    Add categories

--- _data/reviews.js
+++ _data/reviews.js
@@ -8,7 +8,7 @@ import { DateTime } from "luxon"; // Luxon is included with Eleventy
  * @property {string} dateExperienced
  * @property {number} rating
  * @property {boolean} [finished]
- * @property {string[]} category
+ * @property {string[]} categories
  * @property {string[]} comments
  */
 
@@ -22,7 +22,7 @@ const reviewsRaw = [
     dateAdded: "",
     dateExperienced: "",
     rating: ,
-    category: [],
+    categories: [],
     comments: []
   },
   */
@@ -33,7 +33,7 @@ const reviewsRaw = [
     dateExperienced: "2025-07-29",
     rating: 2,
     finished: true,
-    category: ["drinks", "coffee"],
+    categories: ["drinks", "coffee"],
     comments: [
       "I guess I'm just not an oat milk fan. And not enough horchata-ness for me.",
     ],
@@ -44,7 +44,7 @@ const reviewsRaw = [
     dateExperienced: "2025-07-25",
     rating: 3,
     finished: false,
-    category: ["entertainment", "movie", "action"],
+    categories: ["entertainment", "movie", "action"],
     comments: [
       "The 20-ish minute long take is technically impressive, but call me old-fashioned: I like a good serious of cuts.",
       "Also, the last bit of the movie is the most action-for-action's-sake sequence I've maybe ever seen. I bailed on it."
@@ -56,7 +56,7 @@ const reviewsRaw = [
     dateExperienced: "2025-07-12",
     rating: 3,
     finished: true,
-    category: ["entertainment", "movie", "action"],
+    categories: ["entertainment", "movie", "action"],
     comments: [
       "The procedural action was fine. The organized assassin syndicate was my favorite part.",
     ],
