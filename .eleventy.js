02a8f616c5521d3d4dad2734c4107912797e33f4
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-07-28 21:39:03 -0400

    Add Vento

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,26 @@
+import { VentoPlugin } from "eleventy-plugin-vento";
+
 export default async function (eleventyConfig) {
+  eleventyConfig.addPlugin(VentoPlugin, {
+    // An array of Vento plugins to use when compiling
+    plugins: [],
+
+    // Enable/disable Eleventy Shortcodes, Paired Shortcodes,
+    // and Filters in .vto templates
+    shortcodes: true,
+    pairedShortcodes: true,
+    filters: true,
+
+    // Define tags that should be trimmed, or set to true
+    // to trim the default tags (see section on Auto-trimming)
+    autotrim: false,
+
+    // A Vento configuration object
+    ventoOptions: {
+      includes: eleventyConfig.directories.includes,
+    },
+  });
+
   return {
     dir: {
       data: "../_data",
